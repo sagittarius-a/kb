80be971af2051ac205562f16bb338cc8c20ad685
Sagittarius-a <humnoplease@no.pe>
2020-05-01 01:42:13 +0200

    Add: Rewrite kb with hopefully more idiomatic and safer code
    kb has been refactored in order to be hopefully more idiomatic.
    Moreover, some further checks are performed on commands executed.
    Errors have now custom errors messages in order to be as precise as
    possible.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use anyhow::{anyhow, Result};
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
 use std::str;
 
@@ -14,7 +16,7 @@ const ACTIVATED_LAYOUTS: [&str; 2] = ["us", "fr"];
 /// Set the layout to the next layout available
 /// It will use a predefined set of layouts if no configuration is found in the
 /// LAYOUTS environment variable.
-fn next_layout() {
+fn next_layout() -> Result<()> {
     // Check if user supplied a set of layouts different than the default one
     let key = "LAYOUTS";
     let mut layouts = Vec::new();
@@ -32,83 +34,125 @@ fn next_layout() {
     }
 
     // Find the current layout amongst layouts available if any. Exit otherwise.
-    let current = get_layout();
+    let current = get_layout()?;
     let index = if let Some(current_layout) = layouts.iter().position(|e| e == &current) {
         current_layout
     } else {
-        eprintln!("Current layout not found in available layouts. Exiting");
-        return;
+        return Err(anyhow!(
+            "Current layout not found in available layouts. Exiting"
+        ));
     };
 
     // Apply the new keyboard layout
     match index {
-        x if x == layouts.len() - 1 => set_layout(&layouts[0]),
-        _ => set_layout(&layouts[index + 1]),
-    }
+        x if x == layouts.len() - 1 => set_layout(&layouts[0])?,
+        _ => set_layout(&layouts[index + 1])?,
+    };
+
+    Ok(())
 }
 
 /// Write the current layout value to disk. This function does not support the '~'
-/// `KEYBOARD_LAYOUT_FILE` environment variable
-fn write_layout(layout: &str) {
+/// in the `KEYBOARD_LAYOUT_FILE` environment variable
+fn write_layout(layout: &str) -> Result<()> {
     // Fetch home directory
     let home_key = "HOME";
     let home;
     match env::var(home_key) {
         Ok(val) => home = val,
         Err(_e) => {
-            eprintln!("Could not fetch HOME environment variable");
-            return;
+            return Err(anyhow!("Could not fetch {} environment variable", home_key));
         }
     }
 
-    let default_location = Path::new(&home).join(".layout");
+    let mut default_path = PathBuf::from(&home);
+    default_path.push(".layout");
+    let default_location = default_path.to_str().unwrap();
 
-    // Check if user supplied a path different than the default one
+    // Check if user supplied a path different than the default one in the
+    // dedicated environment variable
     let key = "KEYBOARD_LAYOUT_FILE";
-    let filepath;
+    let filepath: std::string::String;
     match env::var(key) {
         Ok(val) => filepath = val,
-        Err(_e) => filepath = String::from(default_location.to_str().unwrap()),
+        Err(_e) => filepath = default_location.into(),
     }
 
-    let mut buffer = File::create(Path::new(&filepath)).unwrap();
-
-    buffer.write_all(layout.as_bytes()).unwrap();
+    // Write the new layout to the appropriate file
+    File::create(Path::new(&filepath))?.write_all(layout.as_bytes())?;
+    Ok(())
 }
 
 /// Get the current keyboard layout
-fn get_layout() -> String {
-    let output = Command::new("setxkbmap")
-        .arg("-query")
-        .output()
-        .expect("failed to execute process");
+fn get_layout() -> Result<String> {
+    let command = "setxkbmap";
+    let arg = "-query";
+    let cmd = match Command::new(command).arg(arg).output() {
+        Ok(s) => s,
+        Err(_) => {
+            return Err(anyhow!(
+                "Failed to get current layour with command '{}'",
+                format!("{} {}", command, arg)
+            ))
+        }
+    };
 
-    let mut current_layout = "";
-    for line in str::from_utf8(&output.stdout)
-        .unwrap()
+    // Make sure the command returned a 0 exit code
+    if !cmd.status.success() {
+        return Err(anyhow!(
+            "Error executing command '{}' with exit code {}. Here is the error message:\n{}",
+            format!("{} {}", command, arg),
+            cmd.status.code().unwrap(),
+            str::from_utf8(&cmd.stderr)?.trim()
+        ));
+    }
+
+    // Iterate over command line output to find the current layout
+    let mut current_layout: Option<&str> = None;
+    for line in str::from_utf8(&cmd.stdout)?
         .split('\n')
         .collect::<Vec<&str>>()
     {
+        // Extract current layout value
         if line.contains("layout") {
-            current_layout = line
-                .split_whitespace()
-                .collect::<Vec<&str>>()
-                .last()
-                .unwrap();
+            match line.split_whitespace().collect::<Vec<&str>>().last() {
+                Some(l) => current_layout = Some(l),
+                None => (),
+            }
         }
     }
 
-    String::from(current_layout)
+    match current_layout {
+        Some(layout) => Ok(String::from(layout)),
+        None => Err(anyhow!("Layout not specified in command output")),
+    }
 }
 
 /// Set the keyboard layout to a user specified value
-fn set_layout(layout: &str) {
-    println!("Setting keyboard layout to {}", layout);
-    Command::new("setxkbmap")
-        .arg(layout)
-        .spawn()
-        .expect("Failed to execute command to set layout");
-    write_layout(layout);
+fn set_layout(layout: &str) -> Result<()> {
+    let command = "setxkbmap";
+    let cmd = match Command::new(command).arg(layout).output() {
+        Ok(s) => s,
+        Err(_) => {
+            return Err(anyhow!(
+                "Failed to set keyboard layout with command '{}'",
+                command
+            ))
+        }
+    };
+
+    // Make sure the command returned a 0 exit code
+    if cmd.status.success() {
+        write_layout(layout)?;
+        Ok(())
+    } else {
+        Err(anyhow!(
+            "Error executing command '{}' with exit code {}. Here is the error message:\n{}",
+            command,
+            cmd.status.code().unwrap(),
+            str::from_utf8(&cmd.stderr)?.trim()
+        ))
+    }
 }
 
 /// Manage command line arguments
@@ -146,16 +190,16 @@ fn manage_args() -> ArgMatches<'static> {
         .get_matches()
 }
 
-fn main() {
+fn main() -> Result<()> {
     let matches = manage_args();
 
     if matches.is_present("get-layout") {
-        println!("{}", get_layout());
-    } else if matches.value_of("set-layout").is_some() {
-        set_layout(matches.value_of("set-layout").unwrap());
+        Ok(println!("{}", get_layout()?))
+    } else if let Some(layout) = matches.value_of("set-layout") {
+        set_layout(layout)
     } else if matches.is_present("next-layout") {
-        next_layout();
+        next_layout()
     } else {
-        println!("{}", get_layout());
+        Ok(println!("{}", get_layout()?))
     }
 }
